662e2a8d3687e7c7fee8ef06009d7ae67450c846
Jan Juarez <janlolsas272272@gmail.com>
2021-05-21 02:43:30 -0500

    Cambios de tab

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,3 +1,7 @@
 function index(){
     document.getElementById('div').innerHTML = "En esta página se hace registro de cosas variadas muy shidoris"
 }
+function iniSesion(){
+    document.getElementById('div').innerHTML = "Usuario: <input class='usu' type='text' placeholder='usuario'/> <br>"+
+                                "Contraseña: <input class='pass' id='pass' type='password' placeholder='contraseña' />";
+}
