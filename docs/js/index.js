edfc7f4cd34a631b419bae908b6a84bea7a11db1
Jan Juarez <janlolsas272272@gmail.com>
2021-05-23 01:32:31 -0500

    Agregada conexión con firebase

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,3 +1,19 @@
+// Set the configuration for your app
+// TODO: Replace with your project's config object
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyACT3iN-MlIjnmz_fUdVuOEmhdTa0i-XWY",
+    authDomain: "mishis-90e0e.firebaseapp.com",
+    projectId: "mishis-90e0e",
+    storageBucket: "mishis-90e0e.appspot.com",
+    messagingSenderId: "322242581078",
+    appId: "1:322242581078:web:e0613936f075f12d2de1f7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+var database = firebase.database();
 function index(){
     document.getElementById('div').innerHTML = "En esta página se hace registro de cosas variadas muy shidoris";
 }
@@ -7,15 +23,15 @@ function iniSesion(){
                                 "<input type='button' name='Button' value='Iniciar Sesión' onClick='sesion(this.form)'>"+
                                 "</form>";
 }
-function crear(){
-    document.getElementByName('reg').style.visibility = "hidden";
-}
 function sesion(form){
     console.log(form.usu.value);
     if(form.usu.value === "asd"){
         if(form.pass.value === "asd"){
             console.log("Sesion iniciada")
             document.getElementById('div').innerHTML = "Sesión de: " + form.usu.value;
+            document.getElementById('nav').innerHTML = "<button name= 'ini' class='btn' onclick='javascript:index()'>Inicio</button>" 
+            + "<button id='reg' class='btn'>Registros</button>"
+            + "<button name='cer' class='btncer'>Cerrar sesión</button>"
         }
     }
 }
