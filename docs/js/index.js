b3b4205748aea6192ba8c283837422ba5033ea99
Jan Juarez <janlolsas272272@gmail.com>
2021-05-23 00:35:41 -0500

    Index mejorado y funciones provisionales de login

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,7 +1,21 @@
 function index(){
-    document.getElementById('div').innerHTML = "En esta página se hace registro de cosas variadas muy shidoris"
+    document.getElementById('div').innerHTML = "En esta página se hace registro de cosas variadas muy shidoris";
 }
 function iniSesion(){
-    document.getElementById('div').innerHTML = "Usuario: <input class='usu' type='text' placeholder='usuario'/> <br>"+
-                                "Contraseña: <input class='pass' id='pass' type='password' placeholder='contraseña' />";
+    document.getElementById('div').innerHTML = "<form id='formi'>Usuario: <input class='usu' name='usu' type='text' placeholder='usuario'/> <br>"+
+                                "Contraseña: <input class='pass' name='pass' type='password' placeholder='contraseña' /><br>"+
+                                "<input type='button' name='Button' value='Iniciar Sesión' onClick='sesion(this.form)'>"+
+                                "</form>";
+}
+function crear(){
+    document.getElementByName('reg').style.visibility = "hidden";
+}
+function sesion(form){
+    console.log(form.usu.value);
+    if(form.usu.value === "asd"){
+        if(form.pass.value === "asd"){
+            console.log("Sesion iniciada")
+            document.getElementById('div').innerHTML = "Sesión de: " + form.usu.value;
+        }
+    }
 }
