dfad87b25a6d761df36daac5001e7a9fb8d3ad6c
Jan Juarez <janlolsas272272@gmail.com>
2021-05-23 20:58:26 -0500

    Registro de tareas

--- docs/js/index.js
+++ docs/js/index.js
@@ -8,30 +8,72 @@ const firebaseConfig = {
     storageBucket: "mishis-90e0e.appspot.com",
     messagingSenderId: "322242581078",
     appId: "1:322242581078:web:e0613936f075f12d2de1f7"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 // Get a reference to the database service
 var database = firebase.database();
+database.ref('Usuarios/'+'Jan').set({
+    nom: 'Jan',
+    data: {
+        email: 'Jan@asd.asd',
+        pass: 'asd',
+        reg: [{nom: 'comer', des: 'comer'}, {nom: 'dormir', des: 'dormir'}]
+    }
+});
 function index(){
     document.getElementById('div').innerHTML = "En esta página se hace registro de cosas variadas muy shidoris";
 }
 function iniSesion(){
     document.getElementById('div').innerHTML = "<form id='formi'>Usuario: <input class='usu' name='usu' type='text' placeholder='usuario'/> <br>"+
                                 "Contraseña: <input class='pass' name='pass' type='password' placeholder='contraseña' /><br>"+
-                                "<input type='button' name='Button' value='Iniciar Sesión' onClick='sesion(this.form)'>"+
+                                "<input type='button' name='Button' value='Iniciar Sesión' onclick='sesion(this.form)'>"+
                                 "</form>";
 }
 function sesion(form){
     console.log(form.usu.value);
     if(form.usu.value === "asd"){
         if(form.pass.value === "asd"){
+            sessionStorage.setItem("usu", form.usu.value);
             console.log("Sesion iniciada")
             document.getElementById('div').innerHTML = "Sesión de: " + form.usu.value;
             document.getElementById('nav').innerHTML = "<button name= 'ini' class='btn' onclick='javascript:index()'>Inicio</button>" 
-            + "<button id='reg' class='btn'>Registros</button>"
+            + "<button id='reg' class='btn' onclick='registro()'>Registros</button>"
             + "<button name='cer' class='btncer'>Cerrar sesión</button>"
         }
     }
+}
+function registro(){
+    document.getElementById('div').innerHTML = "<form name='form2'>Actividad:<input type='text' name='nom' placeholder='Nombre de la actividad'><br>"
+        +"Descripción<input type='text' name='des' placeholder='Descripcion de la actividad'><br>"
+        +"<input type='button' value='Registrar actividad' onclick='regAct(this.form)'></form>"
+        + "<div id='registros'></div>"   
+    existentes();
+}
+function regAct(form){
+    
+    var a=[];        
+    var nom = form.nom.value, des = form.des.value;
+    
+    database.ref("Usuarios/" + "Jan" + "/data/reg").get().then(x => {
+        x.val().forEach(y => {
+            a.push(y);
+        });
+        a.push({nom: nom, des: des});
+    }).then(() => {
+        database.ref("Usuarios/" + "Jan" + "/data/reg").set(a).then(existentes())
+    });
+}
+function existentes(){
+    database.ref("Usuarios/"+"Jan"+"/data/reg").get().then((snapshot)=>{
+        if(snapshot){
+            var reg = "Registros actuales: <br>";
+            console.log(snapshot.val());
+            for(x of snapshot.val()){
+                reg += "Actividad: " + x.nom + "<br>"; 
+            }
+                
+            document.getElementById('registros').innerHTML = reg;
+        }else{console.log("No existe")}
+    })
 }
