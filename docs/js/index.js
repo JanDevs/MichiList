5d39651cb137f057e8a4fd4e913dc19a7a67c08c
Jan Juarez <janlolsas272272@gmail.com>
2021-06-17 15:24:04 -0500

    Ya modifica perfil y las imagenes tienen tamaño que no mata mi pc

--- docs/js/index.js
+++ docs/js/index.js
@@ -42,20 +42,17 @@ function indexPost(){
             + "<input type='file' name='img' id='file'><br>"
             + "<input type='button' id='subPost' value='Subir post' onClick='publiPost(this.form)'/>"
             + "</form></div>"
-    db.collection("posts").orderBy("Fecha", "desc").get().then((querySnapshot) => {
+    db.collection("posts").limit(30).orderBy("Fecha", "desc").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             cosa += `<div>${doc.data().Fecha}<br>
-                        ${doc.data().Texto}<br>
+                        ${doc.data().Usu}: ${doc.data().Texto}<br>
                         <img id='img${ind}' src=""/></div><br>`;
             imagenes.push(doc.data().imagen);
             ind++;
         });
     }).then(() => {
-        console.log(cosa);
         document.getElementById('div').innerHTML = cosa;
     }).then(() => {
-        console.log("a");
-        console.log(imagenes);
         imagenes.forEach((x, y) => {
             storageRef.child(x).getDownloadURL().then(z => {document.getElementById("img"+y).src = z});
         })
@@ -63,7 +60,7 @@ function indexPost(){
 }
 function publiPost(form){
     var txt = form.txtPost.value;
-    var email = "a@a.com"
+    var usu = sessionStorage.getItem('nomUsu');
     var img = form.img.files[0];
     let id = Math.floor(Math.random() * 1092398349);
     let rut = '/mishis/' + img.name + id + '/' + img.name;    
@@ -73,7 +70,7 @@ function publiPost(form){
     db.collection('posts').doc().set({
         Fecha: fecha,
         Texto: txt,
-        Usu: email,
+        Usu: usu,
         imagen: rut
     }).finally(setTimeout(indexPost(), 1500));
     
@@ -88,82 +85,100 @@ function iniSesion(){
 function sesion(form){
     var usuario = form.usu.value;
     var pass = form.pass.value;
-    firebase.auth().signInWithEmailAndPassword(usuario, pass).then((userCredential) => {
-        console.log(userCredential.user);
+    firebase.auth().signInWithEmailAndPassword(usuario, pass).then(() => {
+        db.collection('Usuarios').where('email', '==', usuario).get().then(x => {x.forEach((y)=>{
+            sessionStorage.setItem('cor', y.data().email);
+            sessionStorage.setItem('nomUsu', y.data().nomUsu);
+        })})
         console.log("Sesion iniciada")
-        document.getElementById('div').innerHTML = "Sesión de: " + form.usu.value;
+        document.getElementById('div').innerHTML = "Sesión de: " + sessionStorage.getItem('nomUsu');
         document.getElementById('nav').innerHTML = "<button name= 'ini' class='btn' onclick='javascript:indexPost()'>Inicio</button>" 
-                + "<button id='reg' class='btn' onclick='registro()'>Registros</button>"
+                + "<button id='perfil' class='btn' onclick='perfil()'>Editar perfil</button>"
                 + "<button name='cer' class='btncer'>Cerrar sesión</button>";
     }).catch((error) => {
         console.log(error);
     })
 }
 function nUsu(){
-    document.getElementById('div').innerHTML = "<form>Usuario: <input type='text' name='nom' placeholder='Inserte nombre de usuario'><br>"
+    document.getElementById('div').innerHTML = "<form>Correo: <input type='email' name='nom' placeholder='Inserte correo'><br>"
+                                    + "Nombre de usuario: <input type='text' name='usu' placeholder='Inserte nombre de usuario'><br>"
                                     + "Contraseña: <input type='text' name='pass' placeholder='Ingrese la contraseña'> <br>"
                                     + "Confirmar contraseña: <input type='password' name='conf' placeholder='Confirme contraseña'><br>s"
                                     + "<input type='button' value='Registrar' onclick='regUsu(this.form)'></form>";
 }
 function regUsu(form){
     var usuario = form.nom.value;
+    var usu = form.usu.value;
     var pass = form.pass.value;
     if(pass === form.conf.value){
-        firebase.auth().createUserWithEmailAndPassword(usuario, pass).then((userCredential) => {
-            // Signed in
+        firebase.auth().createUserWithEmailAndPassword(usuario, pass).then(() => {
             alert("Usuario registrado exitosamente");
-            // ...
+        }).then(()=> {
+            db.collection('Usuarios').doc().set({
+                email: usuario,
+                nomUsu: usu
+            }).catch((error) => console.log(error))
         }).catch((error) => {
             console.log(error);
-            // ..
         });
     }else{
         alert("Las contraseñas no coinciden");
     }   
 }
-function registro(){
-    document.getElementById('div').innerHTML = "<form name='form2'>Actividad:<input type='text' name='nom' placeholder='Nombre de la actividad'><br>"
-        +"Descripción<input type='text' name='des' placeholder='Descripcion de la actividad'><br>"
-        +"<input type='button' value='Registrar actividad' onclick='regAct(this.form)'></form>"
-        + "<div id='registros'></div>"   
-    existentes();
-}
-function regAct(form){
-    
-    var a=[];        
-    var nom = form.nom.value, des = form.des.value;
-    
-    database.ref("Usuarios/" + sessionStorage.getItem('usu') + "/data/reg").get().then(x => {
-        if(x.exists()){
-            x.val().forEach(y => {
-                a.push(y);
-            });
-            a.push({nom: nom, des: des});            
-        }else{
-            a.push({nom: nom, des: des});
-        }
-        
-    }).then(() => {
-        database.ref("Usuarios/" + sessionStorage.getItem('usu') + "/data/reg").set(a).then(existentes())
-    });
-}
-function existentes(){
-    database.ref("Usuarios/"+ sessionStorage.getItem('usu') +"/data/reg").get().then((snapshot)=>{
-        if(snapshot.exists()){
-            var reg = "Registros actuales: <br>";
-            console.log(snapshot.val());
-            for(x of snapshot.val()){
-                reg += "Actividad: " + x.nom + "<br>"; 
-            }
-                
-            document.getElementById('registros').innerHTML = reg;
-        }else{console.log("No existe")}
-    })
-}
 function getPost(){
     database.ref("Post").get().then((p) => {
         if(p.exists()){
-
         }
     })
+}
+function perfil(){
+    document.getElementById('div').innerHTML = `Actualizar perfil<br>
+                                                <form>
+                                                Ingresa correo actual: <input type='email' name='emailConf' required><br>
+                                                Nuevo Correo: <input type='email' name='email'><br>
+                                                Nombre de usuario: <input type='text' name='nom' placeholder='${sessionStorage.getItem('nomUsu')}'><br>
+                                                Ingresa tu contraseña: <input type='password' name='conf' required><br>
+                                                Contraseña nueva: <input type='password' name='pass'><br>
+                                                <input type='button' value='confirmar' onclick='actualizaPerfil(this.form)'>
+                                                </form>`                                        
+}
+function actualizaPerfil(form){
+    var cor = form.email.value;
+    var nom = form.nom.value;
+    var pass = form.pass.value;
+    var conf = form.conf.value;
+    var corconf = form.emailConf.value;
+    firebase.auth().signInWithEmailAndPassword(corconf, conf).then(credencial => {
+        alert("Reautenticacion");
+        if(cor != null && cor != ""){
+            firebase.auth().currentUser.updateEmail(cor).then(alert("Correo actualizado por: " + cor)).catch(e => console.log(e));
+            sessionStorage.setItem('cor', cor);
+        }else if(nom != null && nom != ""){
+            db.collection('Usuarios').where('email', '==', sessionStorage.getItem('cor')).get().then(x => {
+                x.forEach(y => {
+                    y.update({
+                        nomUsu: nom,
+                    });
+                });
+                sessionStorage.setItem('nomUsu', nom);
+            }).then(alert("Usuario actualizado por: " + nom)).catch(e => console.log(e));
+        }else if(pass != null && pass != ""){
+            firebase.auth().currentUser.updatePassword(pass).then(alert("Contraseña actualizada")).catch(e => console.log(e));
+        }
+    }).then(()=> setTimeout(()=>{
+        if(cor != null && cor != ""){
+            if(pass != null && pass != ""){
+                firebase.auth().signInWithEmailAndPassword(cor, pass)
+            }else{
+                firebase.auth().signInWithEmailAndPassword(cor, conf)
+            }
+        }else if(pass != null && pass != ""){
+            firebase.auth().signInWithEmailAndPassword(corconf, pass)
+        }else{
+            firebase.auth().signInWithEmailAndPassword(corconf, conf)
+        }
+    },4000)).then(setTimeout(() => document.getElementById('div').innerHTML = `<h1>Datos actualizados correctamente c:</h1>`, 1000));
+    
+    
+    
 }
